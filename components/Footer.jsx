94edcc4bb049ef87d85dd2bf2b6325e21b3a09b2
ShuhaiYu <1141071640@qq.com>
2025-04-22 16:12:00 +1000

    chore: update image

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Footer() {
     return (
-      <footer className="bg-black/90 text-center py-4">
+      <footer className="bg-[#1f1a17] text-center py-4">
         <Image src="/logo_dark.png" alt="Logo" width={150} height={50} className="mx-auto mb-4" />
         <p className="text-sm text-gray-300">Copyright © {new Date().getFullYear()} PURE LIFE MASSAGE. All rights reserved.</p>
       </footer>
